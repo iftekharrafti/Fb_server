ca7a970903e012d51fd4e1a208599a36c50d2531
Iftekhar Rafti <iftekharrafti7040@gmail.com>
2022-02-14 16:51:02 +0600

    added

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express();
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://mydbuser1:06eSisrfDsG77zkF@cluster0.ts3db.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+      await client.connect();
+      const facebook = client.db("facebook");
+      const usersCollection = facebook.collection("users");
+      
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const result = await usersCollection.insertOne(newUser);
+            console.log('got new user the post', req.body);
+            console.log('added user', result);
+            res.json(result)
+        })
+
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(` listening port:${port}`)
+})
