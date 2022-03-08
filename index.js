a320438caf766087dc07c41493d70535bf01bf80
Iftekhar Rafti <iftekharrafti7040@gmail.com>
2022-03-08 12:21:43 +0600

    database change

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express();
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://mydbuser1:06eSisrfDsG77zkF@cluster0.ts3db.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ts3db.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
